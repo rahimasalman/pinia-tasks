05fb5f890de037dbd9b69a9d929f31d4f8d1f86f
rahimasalman <rehimesalman7@gmail.com>
2024-04-02 17:56:25 +0400

    feat: add getters

--- src/App.vue
+++ src/App.vue
@@ -6,8 +6,21 @@
       <h1>Pinia task</h1>
 
     </header>
-    <div class="task-list">
+    <!--    filter-->
+    <nav class="filter">
+      <button @click="filter = 'all'">All tasks</button>
+      <button @click="filter = 'fav'">Favorite tasks</button>
+    </nav>
+    <div class="task-list" v-if="filter === 'all'">
+      <p>All Tasks</p>
       <div v-for="task in taskStore.tasks" :key="task.id">
+        <task-details
+            :task="task"/>
+      </div>
+    </div>
+    <div class="task-list" v-if="filter === 'fav'">
+      <p>Favorite Tasks</p>
+      <div v-for="task in taskStore.favoriteTasks" :key="task.id">
         <task-details
             :task="task"/>
       </div>
@@ -17,6 +30,7 @@
 <script>
 import TaskDetails from "@/components/TaskDetails.vue";
 import {useTaskStore} from "@/stores/TaskStore.js";
+import {ref} from "vue";
 
 export default {
 	components: {
@@ -25,7 +39,8 @@ export default {
 	setup() {
 		const taskStore = useTaskStore();
 
-		return {taskStore};
+		const filter = ref("all");
+		return {taskStore, filter};
 	}
 };
 </script>
--- src/stores/TaskStore.js
+++ src/stores/TaskStore.js
@@ -1,11 +1,21 @@
 import {defineStore} from "pinia";
 
-export const useTaskStore = defineStore("taskStore", () => {
-	return {
+export const useTaskStore = defineStore("taskStore", {
+	state: () => ({
 		tasks: [
 			{id: 1, title: "buy some vegetables", isFav: false},
 			{id: 2, title: "write code", isFav: true},
 			{id: 3, title: "cook a meal", isFav: false}
 		],
-	};
+	}),
+	getters: {
+		// Example getter for getting all favorite tasks
+		favoriteTasks() {
+			return this.tasks.filter(task => task.isFav);
+		},
+		// // Getter for getting the total number of tasks
+		// totalTasks(state) {
+		// 	return state.tasks.length;
+		// },
+	},
 });
