2e2595fa049142cd4376bfc8181d234c026872ad
rahimasalman <rehimesalman7@gmail.com>
2024-04-02 17:07:01 +0400

    feat: add some styles and components

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,32 @@
 <template>
-<main>
-<!--  Heading-->
-  <header>
-    <img src="./assets/pinia.svg" alt="pinia-logo" >
-    <h1>Pinia task</h1>
-  </header>
-</main>
-</template>
+  <main>
+    <!--  Heading-->
+    <header>
+      <img src="./assets/pinia.svg" alt="pinia-logo">
+      <h1>Pinia task</h1>
 
+    </header>
+    <div class="task-list">
+      <div v-for="task in taskStore.tasks" :key="task.id">
+        <task-details
+            :task="task"/>
+      </div>
+    </div>
+  </main>
+</template>
+<script>
+import TaskDetails from "@/components/TaskDetails.vue";
+import {useTaskStore} from "@/stores/TaskStore.js";
+
+export default {
+	components: {
+		TaskDetails
+	},
+	setup() {
+		const taskStore = useTaskStore();
+
+		return {taskStore};
+	}
+};
+</script>
 
